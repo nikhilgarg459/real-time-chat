c0da10632ff9ec6edf8b963e3074c1f0b5e8bbb9
Nikhil Garg <nikhilgarg72@gmail.com>
2017-05-05 10:19:16 +0530

    added public folder

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ var app = express();
 var http = require('http');
 var server = http.createServer(app);
 var io = require('socket.io')(server);
+var path = require('path');
+var logger = require('morgan');
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + 'public/index1.html');
 });
+
+
 
 io.on('connection',function(socket){
     console.log("User connected");
-    
+    socket.broadcast.emit('hi');
     socket.on('disconnect',function(){
         console.log("User disconnected");
     });
---
+++ public/main.js
@@ -0,0 +1,23 @@
+ var FADE_TIME = 150; // ms
+  var TYPING_TIMER_LENGTH = 400; // ms
+  var COLORS = [
+    '#e21400', '#91580f', '#f8a700', '#f78b00',
+    '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
+    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+  ];
+
+   // Initialize variables
+  var $window = window;
+  var $usernameInput = document.getElementsByClassName('usernameInput'); // $('.usernameInput'); // Input for username
+  var $messages = $('.messages'); // Messages area
+  var $inputMessage = $('.inputMessage'); // Input message input box
+
+  var $loginPage = $('.login.page'); // The login page
+  var $chatPage = $('.chat.page'); // The chatroom page
+
+  // Prompt for setting a username
+  var username;
+  var connected = false;
+  var typing = false;
+  var lastTypingTime;
+  var $currentInput = $usernameInput.focus();
