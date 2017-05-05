ae72520adbfe06054f96995f4c9cc5b00d16168d
Nikhil <nikhilgarg72@gmail.com>
2017-05-05 12:44:01 +0530

    Added user joined and user left functionality

--- index.js
+++ index.js
@@ -6,25 +6,53 @@ var io = require('socket.io')(server);
 var path = require('path');
 var logger = require('morgan');
 app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname + 'public/index1.html');
+    res.sendFile(__dirname + 'index1.html');
 });
 
 
+// Chatroom
+
+var numUsers = 0;
 
 io.on('connection',function(socket){
-    console.log("User connected");
+    var addedUser = false;
     socket.broadcast.emit('hi');
-    socket.on('disconnect',function(){
-        console.log("User disconnected");
+   
+   socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+
+    socket.on('add user', function(username){
+        if(addedUser) return;
+
+         // we store the username in the socket session for this client
+         socket.username = username;
+         username;
+         ++numUsers;
+         addedUser = true;
+         socket.emit('login', {
+             numUsers: numUsers
+         });
+         console.log(username + " connected");
+
+         socket.broadcast.emit('user joined', {
+             username: socket.username,
+             numUsers: numUsers
+         });
     });
 
-    socket.on('chat message', function(msg){
-        console.log("message ", msg);
-        io.emit('chat message', msg);
-    });
+    // when the client emits 'add user', this listens and executes
 
 });
 
--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
- var FADE_TIME = 150; // ms
+$(function () {
+  var FADE_TIME = 150; // ms
   var TYPING_TIMER_LENGTH = 400; // ms
   var COLORS = [
     '#e21400', '#91580f', '#f8a700', '#f78b00',
@@ -6,14 +7,13 @@
     '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
   ];
 
-   // Initialize variables
-  var $window = window;
-  var $usernameInput = document.getElementsByClassName('usernameInput'); // $('.usernameInput'); // Input for username
+  // Initialize variables
+  var $window = $(window);
+  var $usernameInput = $('.usernameInput'); // Input for username
   var $messages = $('.messages'); // Messages area
   var $inputMessage = $('.inputMessage'); // Input message input box
-
-  var $loginPage = $('.login.page'); // The login page
-  var $chatPage = $('.chat.page'); // The chatroom page
+  var $loginPage = $('.login.page');
+  var $chatPage = $('.chat.page');
 
   // Prompt for setting a username
   var username;
@@ -21,3 +21,124 @@
   var typing = false;
   var lastTypingTime;
   var $currentInput = $usernameInput.focus();
+
+  var socket = io();
+
+  // Prevents input from having injected markup
+  function cleanInput(input) {
+    return $('<div/>').text(input).text();
+  }
+
+  // Number o fparticipants connected message
+  function addParticipantsMessage(data) {
+    var message = '';
+    if (data.numUsers === 1) {
+      message += "there's 1 participant";
+    } else {
+      message += "there are " + data.numUsers + " participants";
+    }
+    log(message);
+  }
+
+  // Adds a message element to the messages and scrolls to the bottom
+  // el - The element to add as a message
+  // options.fade - If the element should fade-in (default = true)
+  // options.prepend - If the element should prepend
+  //   all other messages (default = false)
+  function addMessageElement(el, options) {
+    var $el = $(el);
+
+    // Setup default options
+    if (!options) {
+      options = {};
+    }
+    if (typeof options.fade === 'undefined') {
+      options.fade = true;
+    }
+    if (typeof options.prepend === 'undefined') {
+      options.prepend = false;
+    }
+
+    // Apply options
+    if (options.fade) {
+      $el.hide().fadeIn(FADE_TIME);
+    }
+    if (options.prepend) {
+      $messages.prepend($el);
+    } else {
+      $messages.append($el);
+    }
+    $messages[0].scrollTop = $messages[0].scrollHeight;
+  }
+
+  // Log a message
+  function log(message, options) {
+    var $el = $('<li>').addClass('log').text(message);
+    addMessageElement($el, options);
+  }
+
+  // Sets the client's username
+  function setUsername() {
+    username = cleanInput($usernameInput.val().trim());
+
+    // If the username is valid
+    if (username) {
+      username = cleanInput($usernameInput.val().trim());
+      $loginPage.fadeOut();
+      $chatPage.show();
+      $loginPage.off('click');
+      $currentInput = $inputMessage.focus();
+
+      // Tell the server your username
+      socket.emit('add user', username);
+    }
+  }
+
+  // Keyboard events
+  $window.keydown(function (event) {
+    // Auto-focus the current input when a key is typed
+    if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+      $currentInput.focus();
+    }
+
+    if (event.which === 13) {
+      if (username) {
+        //sendMessage();
+        //socket.emit('stop typing');
+        typing = false;
+      } else {
+        setUsername();
+      }
+    }
+  });
+
+  // Socket events
+
+  // Whenever the server emits 'login', log the login message
+
+  socket.on('login', function (data) {
+    connected = true;
+    var message = "Welcome to real time Chat - ";
+    log(message, {
+      prepend: true
+    });
+    addParticipantsMessage(data);
+  });
+
+  socket.on('user joined', function(data){
+    log(data.username + ' joined');
+    addParticipantsMessage(data);
+  });
+
+  socket.on('user left', function(data){
+    log(data.username + ' left');
+    addParticipantsMessage(data);
+    //removeChatTyping(data);
+  });
+
+  // socket.emit('disconnect',function(){
+  //   log('You have been disconnected');
+  // });
+
+
+});
